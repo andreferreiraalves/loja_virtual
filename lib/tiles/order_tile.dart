43052da4c5d72a2624ecb36fa1c4762a6611cf67
André Ferreira <andreferreira@MacBook-Pro-de-Andre.local>
2019-08-24 20:37:47 -0300

    order tile

---
+++ lib/tiles/order_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OrderTile extends StatelessWidget {
+  final String orderId;
+
+  OrderTile(this.orderId);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
